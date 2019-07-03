1e5cf37a59d11a8f21839f6b1b1e83b119935727
soulget <466933292@qq.com>
2019-07-03 16:53:37 +0800

    q6

---
+++ test.js
@@ -0,0 +1,21 @@
+// let name = 'ConardLi'
+// {
+//   let name = 'code秘密花园'
+//   console.log(name) // Uncaught ReferenceError: name is not defined
+// }
+// for (var i = 0; i < 3; i++) {
+//   setTimeout(() => console.log(i), 1);
+// }
+
+// for (let i = 0; i < 3; i++) {
+//   setTimeout(() => console.log(i), 1);
+// }
+const bird = {
+  size: "small"
+};
+
+const mouse = {
+  name: "Mickey",
+  small: true
+};
+console.log(mouse["small"])
