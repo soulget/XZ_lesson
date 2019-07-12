54979743c17d18478ac2b0d1643cc147e211646f
soulget <466933292@qq.com>
2019-07-12 16:42:13 +0800

    weeked

---
+++ interview/bubble_sort/bubble_sort.js
@@ -0,0 +1,16 @@
+function bubbleSort(arr) {
+  var len = arr.length;
+  for (var i = 0; i < len; i++) {
+      for (var j = 0; j < len - 1 - i; j++) {
+          if (arr[j] > arr[j+1]) {        //相邻元素两两对比
+              var temp = arr[j+1];        //元素交换
+              arr[j+1] = arr[j];
+              arr[j] = temp;
+          }
+      }
+  }
+  return arr;
+}
+const arr = [8,2,5,9,7];
+
+console.log(bubbleSort(arr));
